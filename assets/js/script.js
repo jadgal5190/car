862a2ed8cef3e97dfddfcf6e08694570b3f1f3b6
jadgal5190 <83171173+jadgal5190@users.noreply.github.com>
2022-09-12 17:46:25 +0430

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+setTimeout(() => {
+
+    setTimeout(() => {
+        $("#test").html(35)
+        setTimeout(() => {
+            $("#test").html(67)
+
+            setTimeout(() => {
+                $("#test").html(99)
+
+                setTimeout(() => {
+                    $("#test").html(110)
+
+                    setTimeout(() => {
+                        $("#test").html(140)
+
+                        setTimeout(() => {
+                            $("#test").html(155)
+
+                            setTimeout(() => {
+                                $("#test").html(164)
+
+                                setTimeout(() => {
+                                    $("#test").html(170)
+
+                                    setTimeout(() => {
+                                        $("#test").html(175)
+
+                                        setTimeout(() => {
+                                            $("#test").html(180)
+
+                                            setTimeout(() => {
+                                                $("#test").html(188)
+                                            }, 900);
+                                        }, 900);
+                                    }, 900);
+
+                                }, 900);
+                            }, 900);
+                        }, 900);
+                    }, 900);
+                }, 900);
+
+            }, 1000);
+        }, 1200);
+    }, 900);
+}, 1000);
